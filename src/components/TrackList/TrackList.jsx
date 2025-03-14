59beea75a5f739129196556cd1542f1d2ad8e99d
EdwardW710 <EdwardW710@yahoo.com>
2025-03-14 00:07:21 -0400

    fully functional complete crud

---
+++ src/components/TrackList/TrackList.jsx
@@ -0,0 +1,30 @@
+const TrackList = (props) => {
+
+    return (
+       <div>
+        <h1>Track List</h1>
+        <div>
+            {!props.tracks.length ? (
+                <h2>No Tracks</h2>
+            ) : (
+           <ul>
+             {props.tracks.map((track) => (
+              <li 
+                key={track._id}
+                style={{ cursor: 'pointer', color: "#646CFF" }}
+                onClick={() => props.handleSelect(track)}
+              >
+                  {track.title}
+                </li>
+             ))}
+           </ul>
+            )}
+        </div>
+        <button onClick={props.handleFormView}>
+            {props.isFormOpen ? 'Close Form' : 'New Track'}
+        </button>
+       </div>
+    );
+};
+
+export default TrackList;
